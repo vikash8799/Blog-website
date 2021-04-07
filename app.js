1f0766e5a8a8f6c10a12bfae43e0b406d6f6075e
Vikash <kushwahavicky122@gmail.com>
2021-04-07 17:14:37 +0530

    Added Procfile, .gitignore and updated ports

--- app.js
+++ app.js
@@ -74,7 +74,10 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
